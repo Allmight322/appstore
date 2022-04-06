11dcec8f31e5732f22901096b573bb30259c768d
Игорь <ogigora@mail.ru>
2022-04-07 02:27:53 +0500

    На проверку оболочка

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -2,13 +2,12 @@ import React from 'react';
 import classes from "./Input.module.css"
 import {useState} from "react";
  export const Input = (props) => {
-     const log = props.onChangeHandler
-     const pas = props.onChangeHandler
+     const inputField = props.onChangeHandler
+
 
     /*const[text, setText] = useState();*/
     const onChangeHandler =(targetValue) => {
-        log(targetValue.target.value)
-        pas(targetValue.target.value)
+        inputField(targetValue.target.value)
 
 
     }
