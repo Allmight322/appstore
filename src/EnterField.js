ae4730751857bd24b6ea9000fb44791c362c56e8
Игорь <ogigora@mail.ru>
2022-04-07 16:58:57 +0500

    На проверку оболочка

--- src/EnterField.js
+++ src/EnterField.js
@@ -15,8 +15,8 @@ const EnterField = () => {
     return (
         <div className={classes.panel}>
             <h1>Авторизация</h1>
-            <Input onChangeHandler={setLogin} onShadow={'text'}/>
-            <Input onChangeHandler={setPassword} onShadow={'password'}/>
+            <Input onChangeHandler1={setLogin} onShadow={'text'} onHidden={'Логин'}/>
+            <Input onChangeHandler1={setPassword} onShadow={'password'} onHidden={'Пароль'}/>
             <Button onClickHandler={logPass}/>
         </div>
     );
--- src/Input/Input.js
+++ src/Input/Input.js
@@ -2,21 +2,26 @@ import React from 'react';
 import classes from "./Input.module.css"
 import {useState} from "react";
  export const Input = (props) => {
-     const inputField = props.onChangeHandler
+     const onChangeHandler1 = props.onChangeHandler1
      const onShadow = props.onShadow
+     const onHidden  = props.onHidden
 
 
     /*const[text, setText] = useState();*/
     const onChangeHandler =(targetValue) => {
-        inputField(targetValue.target.value)
+        onChangeHandler1(targetValue.target.value)
     }
 
     return (
-        <div>
-            <input className={classes.content} onChange=
-                {onChangeHandler}
-                   type = {onShadow}
-            />
+        <div className={classes.content}>
+            <input
+                   onChange=
+                    {onChangeHandler}
+                   id='user-name'
+                   placeholder=' '
+                   autoComplete='off'
+                   type = {onShadow}/>
+            <label for='user-name'>{onHidden}</label>
         </div>
     );
 };
